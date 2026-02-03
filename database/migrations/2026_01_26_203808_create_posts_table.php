93da69e4fafd20b126b15810b41f2485394f325b
Josue Morocho <jcabreratmgs@gmail.com>
2026-02-03 09:50:04 -0500

    Add content column to posts table

--- database/migrations/2026_01_26_203808_create_posts_table.php
+++ database/migrations/2026_01_26_203808_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         $table->foreignId('user_id')->constrained()->onDelete('cascade');
         $table->text('contenido');
         $table->string('imagen')->nullable();
+        $table->text('content');
         $table->timestamps();
     });
 }
